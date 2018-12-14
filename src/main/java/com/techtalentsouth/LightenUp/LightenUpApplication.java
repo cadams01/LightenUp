d0f37dd12a5ababcc9da141b53c27ee8fbcdf3b4
Jason Byer <jbyer3@gmail.com>
2018-12-14 15:24:17 -0500

    initial commit

---
+++ src/main/java/com/techtalentsouth/LightenUp/LightenUpApplication.java
@@ -0,0 +1,14 @@
+package com.techtalentsouth.LightenUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightenUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightenUpApplication.class, args);
+	}
+
+}
+
