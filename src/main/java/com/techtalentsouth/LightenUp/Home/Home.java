50264fb99a6f52be6f5346de7f432a825d9fc4cb
Jason Byer <jbyer3@gmail.com>
2018-12-14 15:48:44 -0500

    add anotations to class

--- src/main/java/com/techtalentsouth/LightenUp/Home/Home.java
+++ src/main/java/com/techtalentsouth/LightenUp/Home/Home.java
@@ -1,5 +1,10 @@
 package com.techtalentsouth.LightenUp.Home;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Home {
+	
+	
 
 }
--- src/main/java/com/techtalentsouth/LightenUp/Home/HomeController.java
+++ src/main/java/com/techtalentsouth/LightenUp/Home/HomeController.java
@@ -1,5 +1,8 @@
 package com.techtalentsouth.LightenUp.Home;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class HomeController {
 
 }
