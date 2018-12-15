cfda86c98885e7a765ec2ef19c13764833992f2b
Jason Byer <jbyer3@gmail.com>
2018-12-15 00:35:14 -0500

    create index.html and establish a root route on the controller

--- src/main/java/com/techtalentsouth/LightenUp/Home/HomeController.java
+++ src/main/java/com/techtalentsouth/LightenUp/Home/HomeController.java
@@ -1,8 +1,21 @@
 package com.techtalentsouth.LightenUp.Home;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
+	
+	@Autowired
+	private HomeRepository homeRepository;
+	
+	@GetMapping("/")
+	public ModelAndView index(Home home) {
+		ModelAndView mv = new ModelAndView("home/index");
+		mv.addObject("homes", homeRepository.findAll());
+		return mv;
+	}
 
 }
