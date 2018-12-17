5f28c1a989bb32fc62f4e3f44cfda3e9c5b60873
Kelsey Whisler <kelsey@kelseywhisler.com>
2018-12-17 09:03:36 -0500

    added create function

--- src/main/java/com/techtalentsouth/LightenUp/Home/HomeController.java
+++ src/main/java/com/techtalentsouth/LightenUp/Home/HomeController.java
@@ -2,8 +2,12 @@ package com.techtalentsouth.LightenUp.Home;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+
 
 @Controller
 public class HomeController {
@@ -21,6 +25,20 @@ public class HomeController {
 	public ModelAndView index(Home home) {
 		ModelAndView mv = new ModelAndView("home/index");
 		mv.addObject("homes", homeRepository.findAll());
+		return mv;
+	}
+	
+	@GetMapping("/home/create")
+	public ModelAndView newBlog(Home home) {
+		ModelAndView mv = new ModelAndView("home/create");
+		return mv;
+	}
+	
+	@PostMapping("/home/create")
+	public ModelAndView create(Home home) {
+		ModelAndView mv = new ModelAndView("home/show");
+		Home myHome = homeRepository.save(home);
+		mv.addObject("home", myHome);
 		return mv;
 	}
 
